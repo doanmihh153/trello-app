afbcd75830c663ef643ae4f72284bcc9fc413a69
doanmihh153 <doanmihh1533@gmail.com>
2025-05-22 01:01:53 +0700

    Logic drag card in column -- Done logic ✅

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -17,6 +17,9 @@ import {
     defaultDropAnimationSideEffects,
 } from '@dnd-kit/core';
 import { arrayMove } from '@dnd-kit/sortable';
+// package lodash
+import { cloneDeep } from 'lodash';
+
 import Column from './ListColumn/Column/Column';
 import TrelloCard from './ListColumn/Column/ListCards/Card/Card';
 
@@ -63,8 +66,13 @@ function BoardContent({ board }) {
 
         const { active, over } = event;
 
+        // Kiem tra drag column || drag card
+        if (activeDragItemsType === ACTIVE_DRAG_ITEM_TYPE.CARD) {
+            console.log('drag Card 🙃');
+            return;
+        };
         // Kiểm tra nếu không tồn tại over --> return
-        if (!over) return;
+        if (!active || !over) return;
 
         // Neu dung vi tri drag thi tra ve!
         if ( active.id !== over.id ) {
@@ -122,10 +130,81 @@ function BoardContent({ board }) {
         }),
     };
 
+    // find column:
+    const findColumnByCardId = (cardId) => {
+        return orderedColumnsState.find(column => column.cards.map(card => card._id)?.includes(cardId));
+    };
+
+    // Qua trinh dragend <ket thuc>
+    const handleDragOver = (event) => {
+        console.log('Drag Column 📊');
+        // khong lam gi gi neu dang drag ->
+        if (activeDragItemsType === ACTIVE_DRAG_ITEM_TYPE.COLUMN) return;
+
+        // a --> active ✅ || o --> over ‼️ || e --> event 🚀 --> Xem thi len handleDragEnd 👆🏼
+        const { active, over } = event;
+
+        if (!active || !over) return;
+
+        // drag card pending...
+        const { id: aDraggingCardId, data: { current: aDraggingCardData } } = active; // active - dragging card id/data
+        // interact card down up -- so với đang drag card
+        const { id: oCardId } = over; // over - card id
+
+        // Tim column cua no!
+        const aColumn = findColumnByCardId(aDraggingCardId);
+        const oColumn = findColumnByCardId(oCardId);
+        if (!aColumn || !oColumn) return; // Neu khong ton tai 1 trong 2 thi ko lam gi :)) -> khong bi crash web
+
+        // neu 2 column kha nhau thi chay!
+        if (aColumn._id !== oColumn._id) {
+            setOrderedColumnsState(prevColumns => {
+                // tim vi tri index cua card!
+                const oCardIndex = oColumn?.cards?.findIndex(card => card._id === oCardId);
+                console.log('index card: ', oCardIndex);
+
+                let newCardIndex;
+
+                const isBelowOverItems =
+                    active.rect.current.translated &&
+                    active.rect.current.translated.top > over.rect.to+ over.rect.height;
+
+                const modifier = isBelowOverItems ? 1 : 0;
+                newCardIndex = oCardIndex >= 0 ? oCardIndex + modifier : oColumn?.cards.length + 1;
+
+                // clone Array orderedColumnsState old --> New Array --> update new orderedColumnsState
+                const nextColumn = cloneDeep(prevColumns);
+
+                const nextActiveColumns = nextColumn.find(column => column._id === aColumn._id);
+                const nextOverColumns = nextColumn.find(column => column._id === oColumn._id);
+
+                if (nextActiveColumns) {
+                    // Delete card in column drag active (xoa cac o column active (cu) --> luc keo tha de sang card khac)
+                    nextActiveColumns.cards = nextActiveColumns.cards.filter(card => card._id !== aDraggingCardId);
+
+                    // Update card in new column (cap nhat mang moi)
+                    nextActiveColumns.cardOrderIds = nextActiveColumns.cards.map(card => card._id);
+                };
+
+                if (nextOverColumns) {
+                    // kiem tra ton tai cua card
+                    nextOverColumns.cards = nextOverColumns.cards.filter(card => card._id !== aDraggingCardId);
+                    // Update Card in new Column --> UI
+                    nextOverColumns.cards = nextOverColumns.cards.toSpliced(newCardIndex, 0, aDraggingCardData );
+
+                    // Update Array new
+                    nextOverColumns.cardOrderIds = nextOverColumns.cards.map(card => card._id);
+                };
+                return nextColumn;
+            });
+        };
+    };
+
     return (
         <DndContext
             onDragEnd={handleDragEnd}
             onDragStart={handleDragStart}
+            onDragOver={handleDragOver}
             sensors={sensors}
         >
             {/* Content */}
